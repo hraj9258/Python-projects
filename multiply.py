d1c24e709365ebc8fc60017e266ffba80ecf8857
Himanshu Raj <himanshurajkush@gmail.com>
2019-11-10 14:24:41 +0530

    Multiply without multiplication

---
+++ multiply.py
@@ -0,0 +1,9 @@
+x=int(input("Enter first number:"))
+y=int(input("Enter the second number:"))
+z=0
+count=x
+while count>0:
+    count=count-1
+    z=z+y
+print("The product of",x,"and",y,"is",z)
+#print(product of two number is, x*y)
