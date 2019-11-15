8442cee29ee5716271c9fa0d1b15f138030eba0c
Himanshu Raj <himanshurajkush@gmail.com>
2019-11-15 22:16:06 +0530

    While syntax

---
+++ while-continue.py
@@ -0,0 +1,9 @@
+i = 0
+while i < 6:
+  i += 1
+  if i == 4:
+    continue#it will leave the excution of his parent statement,
+#hear it will skip the letter "4" while excuting the progrm
+  print(i)
+else:
+      print("I is no longer less than 6")
---
+++ while.e.g.py
@@ -0,0 +1,9 @@
+i=int(input("Please enter the first Number:"))
+z=int(input("Please enter the second Number:"))
+while i < z :
+    i+=1
+    print(i)
+    break
+while i >z :
+    i-=1
+    print(i)
