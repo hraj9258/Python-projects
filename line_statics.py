991751ae47926a93e615690c2cd7d46b51bd51b9
Himanshu Raj <himanshurajkush@gmail.com>
2019-11-27 20:31:30 +0530

    Tells the statics of the string.

---
+++ line_statics.py
@@ -0,0 +1,18 @@
+line=input("Enter a line:")
+lowercount=uppercount=0
+digitcount=alphacount=0
+for a in line:
+    if a.islower():
+        lowercount +=1
+    elif a.isupper():
+        uppercount +=1
+    elif a.isdigit():
+        digitcount +=1
+    elif a.isalpha():
+        alphacount +=1
+
+print("Number of upper case letter:",uppercount)
+print("Number of lowercase letters:",lowercount)
+print("Number of alphabet:",alphacount)
+print("Number of digits:",digitcount)
+
