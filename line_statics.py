65519d4f37a1aa9cb4f9e52f49dcc770eed7063f
Himanshu Raj <himanshurajkush@gmail.com>
2019-11-28 21:27:03 +0530

    Added space count capabilityes

--- line_statics.py
+++ line_statics.py
@@ -1,6 +1,6 @@
 line=input("Enter a line:")
 lowercount=uppercount=0
-digitcount=alphacount=0
+digitcount=alphacount=digitspace=0
 for a in line:
     if a.islower():
         lowercount +=1
@@ -10,9 +10,12 @@ for a in line:
         digitcount +=1
     elif a.isalpha():
         alphacount +=1
+    elif a.isspace():
+        digitspace +=1
 
 print("Number of upper case letter:",uppercount)
 print("Number of lowercase letters:",lowercount)
 print("Number of alphabet:",alphacount)
 print("Number of digits:",digitcount)
+print("Number of spaces:",digitspace)
 
