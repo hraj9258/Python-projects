26368482ea306d26df6a8e82a4181439e99e5ee2
Himanshu Raj <himanshurajkush@gmail.com>
2019-12-05 19:53:26 +0530

    Bubble Sort

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+lst=list(input("Please Enter your list:"))
+print("Original list is:",lst)
+n=len(lst)
+
+for i in range(n):
+    for j in range(0,n-i-1):
+        if lst[j]>lst[j+1]:
+            lst[j],lst[j+1]=lst[j+1],lst[j]
+print("List after sorting:",lst)
