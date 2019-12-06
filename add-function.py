96877358097018f07677245b5f83cd56f4f50f1f
Himanshu Raj <himanshurajkush@gmail.com>
2019-12-06 14:24:14 +0530

    Addition through Function

---
+++ add-function.py
@@ -0,0 +1,7 @@
+def calsum(x,y):
+    s=x+y
+    return s
+num1=float(input("Enter first number:"))
+num2=float(input("Enter second number:"))
+x=calsum(num1,num2)
+print("Sum of two given number is:",x)
