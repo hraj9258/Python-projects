3c59dcb9115b8ad530c4d24392ff730e7dbbedd4
Himanshu Raj <himanshurajkush@gmail.com>
2020-01-10 13:10:45 +0530

    To calculate simple interest

--- Simple-interest-fun.py
+++ Simple-interest-fun.py
@@ -4,8 +4,8 @@ def interest(principal,time=2,rate=0.10):
 #__main__
 principal=float(input("Please Enter the Principal Amount:"))
 print("Simple interest with default ROI(0.10) and time(2) Value is:","Rs.",interest(principal/100))
-roi=float(input("Enter Rate of Intrest(ROI):"))
+rate=float(input("Enter Rate of Intrest(ROI):"))
 time=int(input("Enter time in years:"))
 print("Simpel interest with your provided ROI and time value is")
-si2=interest(principal,time,roi/100)
+si2=interest(principal,time,rate/100)
 print("Rs.",si2)
